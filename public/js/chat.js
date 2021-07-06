12e6c92d2b4cfa46eb137989f5686ec28721aed9
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2021-07-06 15:23:54 -0400

    Socket.io Events
    - We created server to clients events.
    For single connection use our socket instance, for general create io instance
    - Emit: emit(<name Emit>, args or callback function).
    - on: Create an event to send a server (in this project).

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,13 @@
-io();
+const socket = io();
+
+// our name event "countUpdated" must be equal that the server event "countUpdated"
+// the name of count must optional.
+socket.on("countUpdated", (count) => {
+  console.log("the count has been updated", count);
+});
+
+document.querySelector("#increment").addEventListener("click", () => {
+  console.log("Clicked");
+  // this emit is from the client.
+  socket.emit("increment")
+});
--- src/index.js
+++ src/index.js
@@ -10,13 +10,30 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 const port = process.env.PORT || 3000;
-
 const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-io.on("connection", () => {
+let count = 0;
+
+// server (emit) -> client (receive) - countUpdated
+// client (emit) -> server (receive) - increment
+
+// socket is an object containing info about socket connection
+io.on("connection", (socket) => {
   console.log("new web socket connection");
+
+  // Event
+  // we pass the count value as callback to return a value.
+  socket.emit("countUpdated", count);
+
+  socket.on("increment", () => {
+    count++;
+    // This is going to emit to a single connection
+    //socket.emit("countUpdated", count);
+    // this is going to emit to every connection
+    io.emit("countUpdated", count);
+  });
 });
 
 // This is for lister our http server
