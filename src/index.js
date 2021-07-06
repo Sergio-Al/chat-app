9fc8e9ec073a53ccacc6d725a75237335f989bbd
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2021-07-05 20:26:18 -0400

    Starting with Socket.io
    - We installed socket io: npm i socket.io.
    - We implement the following on index.js
    - For create our server require('http')
    - We create a server http server with our app express.
    - Now we listen to our server http.
    - For use socket io require('socket.io')
    - we create our socket implementing our server http socketio(server)
    - Create our event with io.on(<name Event>, function).
    - In html we create a new script src='socket.io/socket.io.js'
    - We create a new js file within public/js/chat.js
    - in Chat.js we create a new function io() to access to server.
    - Test our server and now our server have a persistent connection.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 const path = require("path");
+const http = require("http");
 const express = require("express");
-const port = process.env.PORT || 3000;
+const socketio = require("socket.io");
 
 const app = express();
+// this is for create a new server explicitly, beacuse express create a one for us but we haven't access to it.
+const server = http.createServer(app);
+// We pass out created server
+const io = socketio(server);
 
+const port = process.env.PORT || 3000;
 
 const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-// app.get("/", (req, res, next) => {
-//   res.sendFile("index");
-// });
+io.on("connection", () => {
+  console.log("new web socket connection");
+});
 
-app.listen(port, () => {
+// This is for lister our http server
+server.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
