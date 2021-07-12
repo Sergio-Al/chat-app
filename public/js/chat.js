97112bee7d9fec57ca5fd6928cb5d92aba79fc8e
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2021-07-11 21:35:30 -0400

    form and button states
    -We refactored all our code to reference our elements.
    -We create functions to disable our functions to prevent double click functions.

--- public/js/chat.js
+++ public/js/chat.js
@@ -12,20 +12,38 @@ const socket = io();
 //   socket.emit("increment")
 // });
 
-function sendMessage() {
-  const message = document.getElementById("msgInput").value;
+// Elements
+const $messageForm = document.querySelector("#message-form");
+const $messageFormInput = $messageForm.querySelector("input");
+const $messageFormButton = $messageForm.querySelector("button");
+
+$messageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // disable button for prevent double click/send
+  // first param for fieldName, second for value,
+  // in this case the second disables and the first its the name to access later.
+  $messageFormButton.setAttribute("disabled", "disabled");
+  // Remember! the name next elements is from a name property in html (in this case message)
+  const message = e.target.elements.message.value;
+
   if (message === "") {
     console.log("nothing to emit!");
     return;
   }
   socket.emit("sendMessage", message, (error) => {
+    // enable again
+    $messageFormButton.removeAttribute("disabled");
+    $messageFormInput.value = "";
+    $messageFormInput.focus();
+
     if (error) {
       return console.log(error);
     }
 
     console.log("Message Delivered!");
   });
-}
+});
 
 socket.on("receivedMsg", (msg) => {
   console.log("Message: ", msg);
@@ -35,10 +53,15 @@ socket.on("message", (message) => {
   console.log("Message: ", message);
 });
 
-document.querySelector("#send-location").addEventListener("click", () => {
+// Elements for send location
+const $locationButton = document.querySelector("#send-location");
+
+$locationButton.addEventListener("click", () => {
   if (!navigator.geolocation) {
     return alert("Geolocation is not supported by your browser");
   }
+
+  $locationButton.setAttribute("disabled", "disabled");
 
   navigator.geolocation.getCurrentPosition((position) => {
     socket.emit(
@@ -48,6 +71,7 @@ document.querySelector("#send-location").addEventListener("click", () => {
         long: position.coords.longitude,
       },
       (message) => {
+        $locationButton.removeAttribute("disabled");
         console.log("Result", message);
       }
     );
